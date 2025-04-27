9f4efc784e9f7f0daa27dd3536aad001d721be92
uniiquecornnx <devisha18.003gupta@gmail.com>
2025-04-27 16:45:05 +0400

    check check

--- src/MyToken.sol
+++ src/MyToken.sol
@@ -1,4 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol"; 
+
+contract MyToken is ERC20 {
+    uint256 public constant INITIAL_SUPPLY = 1_000_000 * 1 ether;
+
+    constructor() ERC20("boom", "BOOM"){
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+}
